eba122aa80bfafae4ebeb06a33d7db6d7aec732b
Thales Oliveira <mileto.developer@gmail.com>
2019-10-14 07:44:18 -0300

    Incluindo cadastro - binding

---
+++ Cadastro/Cadastro/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.ClientesTableAdapter.Fill(Me.CadastroDataSet.Clientes)
+    End Sub
+
+    Private Sub ClientesBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs)
+        Me.Validate()
+        Me.ClientesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.CadastroDataSet)
+
+    End Sub
+
+    Private Sub ClientesBindingNavigatorSaveItem_Click_1(sender As Object, e As EventArgs) Handles ClientesBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.ClientesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.CadastroDataSet)
+
+    End Sub
+
+    Private Sub btnAbrir_Click(sender As Object, e As EventArgs) Handles btnAbrir.Click
+        My.Forms.Form2.Show()
+    End Sub
+End Class
