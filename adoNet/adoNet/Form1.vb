3858b560ff03cb3f681ab0cb602f0a06cbbeb039
Thales Oliveira <mileto.developer@gmail.com>
2019-10-31 16:45:29 -0200

    atualizando projeto

---
+++ adoNet/adoNet/Form1.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Configuration
+Imports System.Data.SqlClient
+Imports System.Data
+
+Public Class frmManterEstoque
+    Dim connectionString As String = ""
+    Private Sub frmManterEstoque_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'https://www.codeproject.com/Questions/221388/ConfigurationManager-Not-declared
+        connectionString = ConfigurationManager.ConnectionStrings("EstoqueConnectionString").ConnectionString
+
+        exibirProdutos()
+
+    End Sub
+
+    Private Sub btnExibir_Click(sender As Object, e As EventArgs) Handles btnExibir.Click
+        exibirProdutos()
+    End Sub
+
+    Private Sub exibirProdutos()
+        exibirProdutos()
+    End Sub
+
+End Class
