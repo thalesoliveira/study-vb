7e0ce50ec7d2cc43284324ec7d09ccff46f5a86f
Thales Oliveira <mileto.developer@gmail.com>
2019-12-18 17:57:09 -0300

    Atualizando Projeto

---
+++ WindowsForms/WindowsFormsBackGroundWorker/WindowsFormsBackGroundWorker/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.btnIniciar = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.btnCancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(177, 25)
+        Me.txtNumero.Multiline = True
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(155, 34)
+        Me.txtNumero.TabIndex = 0
+        '
+        'btnIniciar
+        '
+        Me.btnIniciar.Location = New System.Drawing.Point(12, 25)
+        Me.btnIniciar.Name = "btnIniciar"
+        Me.btnIniciar.Size = New System.Drawing.Size(148, 34)
+        Me.btnIniciar.TabIndex = 1
+        Me.btnIniciar.Text = "Iniciar Operação"
+        Me.btnIniciar.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(13, 85)
+        Me.ProgressBar1.Maximum = 500
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(319, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'btnCancelar
+        '
+        Me.btnCancelar.Location = New System.Drawing.Point(13, 130)
+        Me.btnCancelar.Name = "btnCancelar"
+        Me.btnCancelar.Size = New System.Drawing.Size(147, 39)
+        Me.btnCancelar.TabIndex = 3
+        Me.btnCancelar.Text = "Cancelar Operação"
+        Me.btnCancelar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(346, 324)
+        Me.Controls.Add(Me.btnCancelar)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnIniciar)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Name = "Form1"
+        Me.Text = "BackGroundWorker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents btnIniciar As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents btnCancelar As Button
+End Class
---
+++ WindowsForms/WindowsFormsBackGroundWorker/WindowsFormsBackGroundWorker/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+
+        For i = 0 To 500 Step 50
+            If BackgroundWorker1.CancellationPending = True Then
+                e.Cancel = True
+                Exit For
+            Else
+                txtNumero.Text = i.ToString()
+                BackgroundWorker1.ReportProgress(i)
+                System.Threading.Thread.Sleep(1000)
+            End If
+        Next
+    End Sub
+
+    Private Sub btnIniciar_Click(sender As Object, e As EventArgs) Handles btnIniciar.Click
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Control.CheckForIllegalCrossThreadCalls = False
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        MessageBox.Show("Operação Finalizada com sucesso!!")
+        txtNumero.Text = ""
+    End Sub
+
+    Private Sub btnCancelar_Click(sender As Object, e As EventArgs) Handles btnCancelar.Click
+        BackgroundWorker1.CancelAsync()
+        BackgroundWorker1.Dispose()
+    End Sub
+End Class
