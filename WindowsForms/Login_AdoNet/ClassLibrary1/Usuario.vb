214a6569d52f6308f016ee34de239bc94c1857cb
Thales Oliveira <mileto.developer@gmail.com>
2020-03-06 18:00:55 -0300

    Adicionando o projeto Login AdoNet

---
+++ WindowsForms/Login_AdoNet/ClassLibrary1/Usuario.vb
@@ -0,0 +1,7 @@
+﻿Public Class Usuario
+
+    Public Property usuarioid As Integer
+    Public Property login As String
+    Public Property senha As String
+
+End Class
---
+++ WindowsForms/Login_AdoNet/DAL/AcessoDB.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+Imports System.Configuration
+Imports DTO
+
+
+Public Class AcessoDB
+
+    Private Shared Function GetDbConnection() As OleDbConnection
+        Try
+            Dim conString As String = ConfigurationManager.ConnectionStrings("USRLDPString").ConnectionString
+            Dim conn As OleDbConnection = New OleDbConnection(conString)
+            conn.Open()
+            Return conn
+
+        Catch ex As Exception
+            Throw ex
+        End Try
+    End Function
+
+    Public Overloads Shared Function GetDataTable(ByVal sql As String) As DataTable
+        Using conn As OleDbConnection = GetDbConnection()
+            Using da As New OleDbDataAdapter(sql, conn)
+                Dim table As New DataTable
+                da.Fill(table)
+                Return table
+            End Using
+        End Using
+    End Function
+
+End Class
